c3db924b43b9f9a866530ccbd91429de1673d21b
julikunz <julia.kunz@rwth-aachen.de>
2022-05-11 18:07:15 +0200

    log

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,6 +3,13 @@
 import connexion
 from swagger_server.controllers import poll
 from swagger_server import encoder
+import logging
+from flask import request
+
+
+def log_req():
+    print("New request")
+    print(request.get_data())
 
 
 def main():
@@ -10,6 +17,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.app.url_map.strict_slashes = False
+    app.app.before_request(log_req)
     app.add_api('swagger.yaml', arguments={
                 'title': 'Swagger Parser Microservice'})
     app.run(port=8081, debug=True)
